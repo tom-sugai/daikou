a1ed399b23adc1f9889bd5dffb0b34e4efbf96b1
Tom Sugai <tom.sugai@gmail.com>
2024-01-20 16:48:27 +0900

    delete needless use Email

--- src/Controller/Component/SendMailComponent.php
+++ src/Controller/Component/SendMailComponent.php
@@ -3,7 +3,6 @@ namespace App\Controller\Component;
 
 use Cake\Controller\Component;
 use Cake\Controller\ComponentRegistry;
-use Cake\Mailer\Email;
 use Cake\Mailer\Mailer;
 
 /**
@@ -17,41 +16,27 @@ class SendMailComponent extends Component
      * Default configuration.
      *
      * @var array
-     */
+    */
     protected $_defaultConfig = [];
 
     public function initialize(array $config): void
     {
-        //$this->email = new Email('default');
         $this->mailer = new Mailer('default');
     }
 
     public function send($message, $order)
     {
-        //debug($this->mailer);
         // send mail
         $this->mailer
             ->setEmailFormat('html')
-            ->setTo('fumiko@svr.home.com')
             ->setFrom('tom@svr.home.com')
-            ->setSubject('Mail test from Smail controller!!')
+            ->setTo('fumiko@svr.home.com')
+            ->setSubject('お買い物リストの確認メールです')
             ->setViewVars(['order' => $order])
             ->viewBuilder()
                 ->setTemplate('welcome')
                 ->setLayout('default');
-    
-        $this->mailer->deliver();
 
-        /** 
-        $this->email
-            ->setTemplate('welcome', 'default')
-            ->emailFormat('html')
-            //->to('fumiko@svr.home.com')
-            ->to($order->user->email)
-            ->from(['tom@svr.home.com' => 'CakePHP'])
-            ->subject('Thank you mail !!')
-            ->viewVars(['order' => $order])
-            ->send($message);
-        */    
+        $this->mailer->deliver();  
     }
 }
