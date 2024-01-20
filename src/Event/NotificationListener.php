e2f04fe190e5a052fb7091a6498ba632f2476a57
Tom Sugai <tom.sugai@gmail.com>
2024-01-20 11:39:39 +0900

    change implementedEvents() Notification.Math to E-Mail

--- src/Event/NotificationListener.php
+++ src/Event/NotificationListener.php
@@ -4,6 +4,8 @@ namespace App\Event;
 use Cake\Event\EventListenerInterface;
 use Cake\Controller\ComponentRegistry;
 use App\Controller\Component\SendMailComponent;
+use App\Controller\Component\MathComponent;
+use Cake\Error\Debugger;
 
 /**
  * Class NotificationListener
@@ -12,19 +14,44 @@ use App\Controller\Component\SendMailComponent;
 class NotificationListener implements EventListenerInterface
 {
     protected $Email;
+    protected $Math;
 
     public function __construct()
     {
         $this->Email = new SendMailComponent(new ComponentRegistry());
+        //$this->Math = new MathComponent(new ComponentRegistry());
     }
     
     public function implementedEvents(): array
     {
+        /** 
+        return [
+            'Notification.Math' => 'mathNotification'
+        ];
+        */
+         
         return [
             'Notification.E-Mail' => 'mailNotification'
         ];
+        
     }
 
+    /**
+     * Math 計算結果の通知
+     * @param $amount1
+     * @param $amount2
+     */
+    /** 
+    public function mathNotification($event, $amount1, $amount2)
+    {
+        //debug($amount1);
+        //debug($amount2)
+        //echo "This is mailNotification";
+        $result = $this->Math->doComplexOperation($amount1, $amount2);
+        debug($result);
+    }
+    */
+    
     /**
      * E-Mail通知処理
      * @param $event
