63384db86e533141a33abfd4900529d14ab019bd
Tom Sugai <tom.sugai@gmail.com>
2024-01-27 15:07:16 +0900

    add div class="topguid" for top page

--- templates/Top/index.php
+++ templates/Top/index.php
@@ -1,7 +1,10 @@
-<?php $this->set('headertext', 'This is headertext in the new_index.ctp file.'); ?>
+<?php $this->set('headertext', 'This is headertext in the Top/index.php file.'); ?>
 
-<div class="sheader">
-        <p><?= "-- This is Page Info Block in the new_index.php file. --" ?></p>
+<div class="topguid">
+    <p><?= "ログインして商品選択へ進んでください" ?></p>
+    <div class="button-row">
+        <?= $this->Html->link(__('ログイン'), ['controller' => 'Users', 'action' => 'login'], ['class' => 'button']) ?>
+    </div>
 </div>
 
 <?= $msg ?>
