556d227f17b2b23ab58df81acdc0394b992a9e10
Tom Sugai <tom.sugai@gmail.com>
2024-01-31 11:36:32 +0900

    display login user to Info Block

--- templates/Carts/check_cart.php
+++ templates/Carts/check_cart.php
@@ -7,7 +7,7 @@
 
 <?php $this->set('headertext', 'headertext from element'); ?>
 <div class="sheader">
-    <p><?= "Info Block in the check_cart.php" ?></p>
+    <p><?= "Info Block in the check_cart.php" . " / " ?><?= "Login User : " . $username ?> </p>
     <div class="button-row">
         <?= $this->Html->link(__('商品選択へ戻る'), ['controller' => 'Items', 'action' => 'newIndex'], ['class' => 'button float-right']) ?>
         <?= $this->Html->link(__('注文商品の確認'), ['controller' => 'Carts', 'action' => 'checkOrder'], ['class' => 'button float-right']) ?>
