dd36f1c17286881f88ddd15cafe1685c9a1babbe
Tom Sugai <tom.sugai@gmail.com>
2024-02-01 18:27:32 +0900

    elementnize class syohin block by syohinbox

--- templates/element/syohinbox.php
+++ templates/element/syohinbox.php
@@ -11,14 +11,4 @@
             <div class="boxE"><?= $item->product->pname ?></div>
             <div class="boxF"><?= "---- Price or Others line -----" . "<br>" ?></div>
         </div>
-    </div>
-    <div class="boxG">
-            <?= $this->Html->link(__('カートに入れる'), ['controller' => 'Carts', 'action' => 'intoCart', $item->id]) ?>
-            <!--
-            <?= $this->Html->link(__('View'), ['action' => 'view', $item->id]) ?>
-            <?= $this->Html->link(__('Edit'), ['action' => 'edit', $item->id]) ?>
-            <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $item->id], ['confirm' => __('Are you sure you want to delete # {0}?', $item->id)]) ?>
-            -->
-    </div>
-    <div class="boxH"><?= "---- fotter line  for each Product----" ?></div>   
-</div>            
+    </div>            
