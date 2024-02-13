300fe4f245318935b1de5c4de85eada7187bdbe1
Tom Sugai <tom.sugai@gmail.com>
2024-02-13 18:59:05 +0900

    replace div scontainer & div pctrl block to this->element(check-cart-list)

--- templates/Carts/check_cart.php
+++ templates/Carts/check_cart.php
@@ -12,27 +12,6 @@
         <?= $this->Html->link(__('注文商品の確認'), ['controller' => 'Carts', 'action' => 'checkOrder'], ['class' => 'button float-right']) ?>
     </div>
 </div>
-<div class="scontainer">
-    <?php //debug($carts); ?>
-    <?php foreach ($carts as $cart): ?>
-        <?php //debug($cart); ?>
-        <?php $this->set('cart', $cart); ?>
-        <?php $this->set('item', $cart->item); ?>
-        <?= $this->element('syohinlist'); ?>
+<?= $this->element('check-cart-list') ?>
 
-        <!--
-        <?= $this->element('syohinbox'); ?>
-        <?= $this->element('act_check_cart'); ?>
-        -->
-    <?php endforeach; ?>
-</div>   
-<div class="pctrl">
-    <ul class="pagination">
-        <!--<?= $this->Paginator->first('<< ' . __('first')) ?>-->
-        <?= $this->Paginator->prev('< ' . __('previous')) ?>
-        <?= $this->Paginator->numbers() ?>
-        <?= $this->Paginator->next(__('next') . ' >') ?>
-        <!--<?= $this->Paginator->last(__('last') . ' >>') ?>-->
-    </ul>
-    <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
-</div>
+
