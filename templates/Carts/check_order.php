88c6ce868e853aafd0e286894de5ee3d90caaf2c
Tom Sugai <tom.sugai@gmail.com>
2024-02-13 19:01:45 +0900

    replase div scontainer & div pctrl block to $this->element('check-order-list')

--- templates/Carts/check_order.php
+++ templates/Carts/check_order.php
@@ -8,36 +8,9 @@
 <div class="sheader">
     <p><?= "Info Block in the check_order.php" . " / " ?><?= "Login User : " . $username ?> </p>
     <div class="button-row">
-        <?= $this->Html->link(__('商品選択へ戻る'), ['controller' => 'Items', 'action' => 'newIndex'], ['class' => 'button10']) ?>
-        <?= $this->Html->link(__('カートへ戻る'), ['controller' => 'Carts', 'action' => 'checkCart'], ['class' => 'button10']) ?>
-        <?= $this->Html->link(__('注文情報の入力'), ['controller' => 'Orders', 'action' => 'fixOrder'], ['class' => 'button10']) ?>
+        <?= $this->Html->link(__('商品選択へ戻る'), ['controller' => 'Items', 'action' => 'newIndex'], ['class' => 'button float-right']) ?>
+        <?= $this->Html->link(__('カートへ戻る'), ['controller' => 'Carts', 'action' => 'checkCart'], ['class' => 'button float-right']) ?>
+        <?= $this->Html->link(__('注文情報の入力'), ['controller' => 'Orders', 'action' => 'fixOrder'], ['class' => 'button float-right']) ?>
     </div>
-    <!--
-    <?= $this->Html->link(__('商品選択へ戻る'), ['controller' => 'Items', 'action' => 'newIndex'], ['class' => 'button float-right']) ?>
-    <?= $this->Html->link(__('カートへ戻る'), ['controller' => 'Carts', 'action' => 'checkCart'], ['class' => 'button float-right']) ?>
-    <?= $this->Html->link(__('注文情報の入力'), ['controller' => 'Orders', 'action' => 'fixOrder'], ['class' => 'button float-right']) ?>
-    -->
 </div>
-<div class="scontainer">
-    <?php //debug($carts); ?>
-    <?php foreach ($carts as $cart): ?>
-        <?php //debug($cart); ?>
-        <?php $this->set('cart', $cart); ?>
-        <?php $this->set('item', $cart->item); ?>
-        <?= $this->element('syohinlist'); ?>
-        <!--
-        <?= $this->element('syohinbox'); ?>
-        <?= $this->element('act_check_order'); ?>
-        -->
-    <?php endforeach; ?>
-</div>
-<div class="pctrl">
-    <ul class="pagination">
-        <!--<?= $this->Paginator->first('<< ' . __('first')) ?>-->
-        <?= $this->Paginator->prev('< ' . __('previous')) ?>
-        <?= $this->Paginator->numbers() ?>
-        <?= $this->Paginator->next(__('next') . ' >') ?>
-        <!--<?= $this->Paginator->last(__('last') . ' >>') ?>-->
-    </ul>
-    <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
-</div>
+<?= $this->element('check-order-list') ?>
