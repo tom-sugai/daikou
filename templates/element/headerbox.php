c1af310b8b9fc1bbadff3b5daf8eaf5fa128d3ff
Tom Sugai <tom.sugai@gmail.com>
2024-02-13 19:17:06 +0900

    delete needless  block gor order

--- templates/element/headerbox.php
+++ templates/element/headerbox.php
@@ -1,20 +1,8 @@
 <?php
     echo $headertext . "<br/>";
-
-
-    /** 
-    $this->Html->addCrumb('Fumiko1','/exc-view/fumiko1');
-    $this->Html->addCrumb('Fumiko2','/exc-view/fumiko2');
-    $this->Html->addCrumb('Fumiko3','/exc-view/fumiko3');
-    $this->Html->addCrumb('Fumiko4','/exc-view/fumiko4');
-    */
 ?>
-<?= $this->Html->link(__('Login'), ['controller' => 'Users', 'action' => 'login']) ?>
-<?= " / " ?>
-<?= $this->Html->link(__('Logout'), ['controller' => 'Users', 'action' => 'logout']) ?>
-<?= " / " ?>
-<!--<?= $this->Html->link(__('Select'), ['controller' => 'Items', 'action' => 'new-index']) ?>-->
-<!--<?= " / " ?>-->
-<!--<?= $this->Html->link(__('CheckCart'), ['controller' => 'Carts', 'action' => 'check-cart']) ?>-->
-<!--<?= " / " ?><?= "<br>" ?>-->
-<!--<?= "Login User : " . $username ?>-->
+<div class="o-loginlogout">
+    <?= $this->Html->link(__('Login'), ['controller' => 'Users', 'action' => 'login']) ?>
+    <?= " / " ?>
+    <?= $this->Html->link(__('Logout'), ['controller' => 'Users', 'action' => 'logout']) ?>
+</div>
