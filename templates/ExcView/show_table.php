c05d020606750bd80903f06d284ba8f46cb45039
Tom Sugai <tom.sugai@gmail.com>
2024-02-20 09:29:42 +0900

    sample table for ExcView/showTable

---
+++ templates/ExcView/show_table.php
@@ -0,0 +1,28 @@
+<?= "table sample" . "<br>" ?>
+<table>
+      <caption>ブレークポイント</caption>
+      <thead>
+        <tr>
+            <th>端末の種類</th><th>水平サイズ</th>
+        </tr>
+      </thead>
+      <tbody>
+        <!--
+        <tr>
+            <td>端末の種類</td><td>水平サイズ</td>
+        </tr>
+        -->
+        <tr>
+           <td>mobile</td><td>≤640px</td>
+        </tr>
+        <tr>
+            <td>tablet</td><td>>640px</td>         
+        </tr>
+        <tr>
+            <td>desktop</td><td>>1280px</td>
+        </tr>
+        <tr>
+            <td>large desktop</td><td>>1920px</td>
+        </tr>
+    </tbody>
+  <table>
