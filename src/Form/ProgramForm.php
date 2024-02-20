e22d050bfb18aebe2039fc1d545035f58d1e360f
Tom Sugai <tom.sugai@gmail.com>
2024-02-20 09:14:07 +0900

    add return data type for _execute()

--- src/Form/ProgramForm.php
+++ src/Form/ProgramForm.php
@@ -38,7 +38,7 @@ class ProgramForm extends Form
      * @param array $data Form data.
      * @return bool
      */
-    protected function _execute(array $data)
+    protected function _execute(array $data): bool
     {
         return true;
     }
