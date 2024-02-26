c3225bf59b13722755dd9294d48f23dcdcce44ea
Tom Sugai <tom.sugai@gmail.com>
2024-02-26 14:44:40 +0900

    button '商品選択へ戻る'　change action items/newIndex to items/otsukai

--- templates/Carts/check_order.php
+++ templates/Carts/check_order.php
@@ -8,7 +8,7 @@
 <div class="sheader">
     <p><?= "Info Block in the check_order.php" . " / " ?><?= "Login User : " . $username ?> </p>
     <div class="button-row">
-        <?= $this->Html->link(__('商品選択へ戻る'), ['controller' => 'Items', 'action' => 'newIndex'], ['class' => 'button float-right']) ?>
+        <?= $this->Html->link(__('商品選択へ戻る'), ['controller' => 'Items', 'action' => 'otsukai'], ['class' => 'button float-right']) ?>
         <?= $this->Html->link(__('カートへ戻る'), ['controller' => 'Carts', 'action' => 'checkCart'], ['class' => 'button float-right']) ?>
         <?= $this->Html->link(__('注文情報の入力'), ['controller' => 'Orders', 'action' => 'fixOrder'], ['class' => 'button float-right']) ?>
     </div>
