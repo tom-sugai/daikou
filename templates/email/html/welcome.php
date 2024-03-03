35b750b344dfc58cef9dc90a712a7b86bf5020e9
Tom Sugai <tom.sugai@gmail.com>
2024-03-03 17:32:21 +0900

    correspond jancode under 493000 products to calculate amount

--- templates/email/html/welcome.php
+++ templates/email/html/welcome.php
@@ -3,14 +3,21 @@
 <?php $total = 0; ?>
 <?php foreach ($order->details as $detail): ?>
 		<img src="<?= $detail->item->product->image ?>"  height="100" width="100" alt=""/>
-		<?= $detail->item->product->pname ?>  
-        <?= $detail->item->product->price ?> * <?= $detail->size ?> ==> 
-        <?php
-		    $uprice = $detail->item->product->price;
-		    $quantity = $detail->size;
-		    $amount = $uprice * $quantity;
-		 ?>
-		<?= "金額 " . $amount . " 円" ?><br>
+		<?php if(($detail->item->jancode - 493000) > 0): ?>
+			<?= $detail->item->product->pname ?>  
+			<?= $detail->item->product->price ?> * <?= $detail->size ?> ==> 
+			<?php
+				$uprice = $detail->item->product->price;
+				$quantity = $detail->size;
+				$amount = $uprice * $quantity;
+			?>
+			<?= "金額 " . $amount . " 円" ?><br>
+		<?php else: ?>
+			<?= $detail->note1 ?>
+			<?= $detail->note2 ?>
+			<?php $amount = $detail->note3 ?>
+			<?= "金額 " . $amount . " 円" ?><br>
+		<?php endif; ?>
 		<?php $total = $total + $amount; ?>
 <?php endforeach; ?>
 <hr></hr>
