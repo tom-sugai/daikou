0793d41db3cf0ffd38d8c3304932a31a20b09753
Tom Sugai <tom.sugai@gmail.com>
2024-03-22 10:38:20 +0900

    add button Go Items/index, Go Exc/View

--- templates/Top/index.php
+++ templates/Top/index.php
@@ -3,6 +3,8 @@
     <p><?= "ログインして商品選択へ進んでください" ?></p>
     <div class="button-row">
         <?= $this->Html->link(__('ログイン'), ['controller' => 'Users', 'action' => 'login'], ['class' => 'button']) ?>
+        <?= $this->Html->link(__('Go Items/index'), ['controller' => 'Items', 'action' => 'otsukai'], ['class' => 'button']) ?>
+        <?= $this->Html->link(__('Go Exc/View'), ['controller' => 'ExcView', 'action' => 'index'], ['class' => 'button']) ?>
     </div>
 </div>
 <p style="font-size:12px;"><?= $msg ?></p>
